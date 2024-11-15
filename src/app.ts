99d10e35d6fbce4bda1b0667327422ba4fe25d2d
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-15 14:47:49 -0300

    Configuração: HTTP server Fastify

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify"
+import cookie from '@fastify/cookie'
+import { usersRoutes } from "./routes/users"
+
+export const app = fastify()
+
+app.register(cookie)
+
+/** Log for requests API */
+app.addHook('preHandler', async (req) => {
+    console.log(`[(${req.method} ${req.url}]`)
+})
+
+app.register(usersRoutes, {
+    prefix: 'users',
+})
