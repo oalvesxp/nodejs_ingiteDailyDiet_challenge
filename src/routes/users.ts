d943684d4217ea51d43290773cdb60e5152c8f3e
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-15 14:58:36 -0300

    Routes: Rota de teste de users

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,4 +2,8 @@ import { FastifyInstance } from "fastify"
 import { knex } from "../database"
 import { z } from "zod"
 
-export async function usersRoutes() {}
+export async function usersRoutes(app: FastifyInstance) {
+    app.get('/', async (req, rep) => {
+        return rep.status(200).send({'Hello': 'World'})
+    })
+}
