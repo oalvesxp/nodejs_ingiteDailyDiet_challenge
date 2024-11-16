5bd2188fbc5e37ee1a392b708ad68c72e9356639
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-16 11:34:43 -0300

    Database: Migration tabela de meals

---
+++ db/migrations/20241116140344_create-meals.ts
@@ -0,0 +1,20 @@
+import type { Knex } from "knex"
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary()
+    table.uuid('user_id').references('users.id').notNullable()
+    table.string('name').notNullable()
+    table.string('description')
+    table.date('date').notNullable()
+    table.boolean('is_on_diet').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
+
