26f324c969f64076078be769d9ddded518994252
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-16 12:42:09 -0300

    Configuração: aspas simples

--- db/migrations/20241116140344_create-meals.ts
+++ db/migrations/20241116140344_create-meals.ts
@@ -1,5 +1,4 @@
-import type { Knex } from "knex"
-
+import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('meals', (table) => {
@@ -13,8 +12,6 @@ export async function up(knex: Knex): Promise<void> {
   })
 }
 
-
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.dropTable('meals')
 }
-
