b46f2c89547e915639a0e91d6b3bc8fa32b18b7a
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-17 17:50:58 -0300

    Routes: Cadastro de refeição

---
+++ src/routes/meals.ts
@@ -0,0 +1,31 @@
+import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
+import { z } from 'zod'
+import { randomUUID } from 'crypto'
+import { checkSessionIdExists } from '../middleware/check-session-id-exists'
+
+export async function mealsRoutes(app: FastifyInstance) {
+  app.post('/', { preHandler: [checkSessionIdExists] }, async (req, rep) => {
+    const createMealBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      isOnDiet: z.boolean(),
+      date: z.coerce.date(),
+    })
+
+    const { name, description, isOnDiet, date } = createMealBodySchema.parse(
+      req.body,
+    )
+
+    await knex('meals').insert({
+      id: randomUUID(),
+      user_id: req.user?.id,
+      name,
+      description,
+      is_on_diet: isOnDiet,
+      date: date.getTime(),
+    })
+
+    return rep.status(201).send()
+  })
+}
